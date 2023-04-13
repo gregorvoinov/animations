74187520eb31923c625fe0b2247b6b34ae13fddd
gregorvoinov <gregorvoinov@gmail.com>
2023-04-13 10:29:34 +0200

    adding gh-pages  module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/animations/' // baseURL: '/<repository>/'
+  },
   css: ["~/assets/css/fonts.css", "~/assets/css/tailwind.css"],
   modules: [
     '@nuxtjs/tailwindcss'
