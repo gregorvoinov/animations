222375a5f71877a2aca010d52ceb1ce96f85391b
gregorvoinov <gregorvoinov@gmail.com>
2023-04-13 10:22:11 +0200

    Updates

--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  css: ["~/assets/css/fonts.css", "~/assets/css/tailwind.css"],
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
-})
